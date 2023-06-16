5113666ba75d12f037fe4d1315f719856e224be9
arhab77 <arhab580@gmail.com>
2023-06-16 17:16:06 +0700

    add tugas basic OOP

---
+++ Tugas_Javascript_OOP/library.js
@@ -0,0 +1,51 @@
+class Siswa {
+    constructor(init) {
+      this.init = init;
+    }
+  
+    createHeader(data) {
+      let open = "<thead><tr>";
+      let close = "</tr></thead>";
+      data.forEach((d) => {
+        open += `<th>${d}</th>`;
+      });
+  
+      return open + close;
+    }
+  
+    createBody(data) {
+      let open = "<tbody>";
+      let close = "</tbody>";
+  
+      data.forEach((d) => {
+        open += `
+          <tr>
+            <td>${d[0]}</td>
+            <td>${d[1]}</td>
+          </tr>
+        `;
+      });
+  
+      return open + close;
+    }
+  
+    render(element) {
+      let table =
+        "<table class='table table-hover'>" +
+        this.createHeader(this.init.columns) +
+        this.createBody(this.init.data) +
+        "</table>";
+      element.innerHTML = table;
+    }
+  }
+  
+  const siswa = new Siswa({
+    columns: ["Name", "Email"],
+    data: [
+      ["Fadhil arhab", "ceritabote@gmail.com"],
+      ["Ahmad Senju", "ahmad@upscale.id"]
+    ]
+  });
+  const app = document.getElementById("app");
+  siswa.render(app);
+  
